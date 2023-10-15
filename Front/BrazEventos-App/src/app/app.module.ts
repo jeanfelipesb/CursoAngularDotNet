993b099e1a6276c2f0d311cb91cbaf0edf006191
Jean Felipe <jean_felipe2008@hotmail.com>
2023-10-15 17:09:20 -0300

    Fim Sessão 5

--- Front/BrazEventos-App/src/app/app.module.ts
+++ Front/BrazEventos-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
@@ -22,7 +24,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/BrazEventos-App/src/app/eventos/eventos.component.ts
+++ Front/BrazEventos-App/src/app/eventos/eventos.component.ts
@@ -8,19 +8,50 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 60;
+  marginImg: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
-  public eventos: any;
 
   public getEventos(): void {
     this.http.get('https://localhost:7045/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = response
+      },
       error => console.log(error)
     );
+  }
+
+  public ControlaExibicaoImagem(): void{
+    this.exibirImagem = !this.exibirImagem;
   }
 
 
