6f2fd3080c63f28fcbbeec13373c6c4fe56c967e
Jean Felipe <jean_felipe2008@hotmail.com>
2023-10-15 13:30:29 -0300

    Add Bootstrap

---
+++ AutenticacaoApiDEV.bat
@@ -0,0 +1,5 @@
+set DIR_API="C:\Users\jean_\Documents\Projetos\Programacao\Curso donet e Angular\Back\BrazEventos.API\bin\Debug\net7.0"
+
+cd %DIR_API%
+
+start "" dotnet BrazEventos.API.dll --urls https://localhost:7045
